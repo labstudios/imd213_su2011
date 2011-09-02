1063e8e1fc6d1b5a238abd1356226b68fa10183c
Brent <labstudios@gmail.com>
2011-09-02 03:54:49 +0000

    Week 8 complete

--- week_6/js/index.js
+++ week_6/js/index.js
@@ -1,8 +1,9 @@
 document.addEvent("domready", function(){
     //this is a cool solution, but a little confusing
-    /*$$(".pane").each(function(el){
+    $$(".pane").each(function(el){
         new WindowPane(el);
-    });*/
-    var win1 = new WindowPane("window1");
+    });
+    /*var win1 = new WindowPane("window1");
     var win2 = new WindowPane("window2");
+    var win3 = new WindowPane("window3");*/
 });
--- week_6/js/windowPane.js
+++ week_6/js/windowPane.js
@@ -18,10 +18,12 @@ var WindowPane = new Class({
         //initial setup
 		this.element = document.id(element);
 		this.setOptions(options);
+        
         //get a handle on the elements I am using
         this.clicker = this.element.getElement("." + this.options.titleClass);
         this.content = this.element.getElement("." + this.options.contentClass);
         
+        //add a click event to my header
         this.clicker.addEvent("click", this.toggle.bind(this));
         //This creates an Fx.Reveal object so we can just call toggle in the click
         this.fx = new Fx.Reveal(this.content, {duration: 1500, mode:'both'});
